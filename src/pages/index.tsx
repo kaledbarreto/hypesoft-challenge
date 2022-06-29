b5f48ab2ea59c4261f7b4855d4c8545464058baf
Kaled Barreto <kaledbarreto@gmail.com>
2022-06-29 20:28:16 -0300

    feat: Initial Configuration

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Head>
+        <title>Homepage</title>
+      </Head>
+      <main>
+        <h1>Hello World</h1>
+      </main>
+    </div>
+  )
+}
+
+export default Home
