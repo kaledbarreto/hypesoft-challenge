dae983e71c4bf4677b1724c7a95067b3d216c3b7
Kaled Barreto <kaledbarreto@gmail.com>
2022-07-02 10:07:16 -0300

    feat: Structured home page in styled components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,49 @@
+import React from 'react'
 import Head from 'next/head'
-import React from 'react';
+import Link from 'next/link'
+import styled from 'styled-components'
 
 const Home: React.FC = () => {
   return (
-    <div>
+    <PageContainer>
       <Head>
-        <title>Homepage</title>
+        <title>Hypesoft - Desafio</title>
       </Head>
-      <main>
-        <h1>Hello World</h1>
-      </main>
-    </div>
+      <NavBar>
+        <Link>
+          <span>About</span>
+        </Link>
+        <Link>
+          <span>Services</span>
+        </Link>
+        <Link>
+          <span>Works</span>
+        </Link>
+        <Link>
+          <span>Contact</span>
+        </Link>
+      </NavBar>
+      <Main>
+        <Headline>
+          Hello, I’m John, a Software Engineer.
+        </Headline>
+        <SubHeadline>
+          Product Designer, UI/UX Designer, and developer based in Brazil. Over the past 17 years, as an art director and designer, I’ve worked with big companies and up-and-coming startups.
+        </SubHeadline>
+        <MainActions>
+          <Link>
+            <ButtonNav>
+              Hire Me
+            </ButtonNav>
+          </Link>
+          <Link>
+            <ButtonNav>
+              View Work
+            </ButtonNav>
+          </Link>
+        </MainActions>
+      </Main>
+    </PageContainer>
   )
 }
 
